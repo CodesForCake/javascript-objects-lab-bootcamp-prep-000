3c18171213eae4be30fb9642e74587e2f95dca37
Mimi Cong <mina123fire@yahoo.com>
2020-08-25 18:04:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
   var newObj = Object.assign({}, object, {[key]:value})
   return newObj;
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  delete object[key];
+  return object;
+}
